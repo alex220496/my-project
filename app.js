e0a8ff548a58a3e20e590151fbe4f910ffd092d1
Alex <focus2204@ukr.net>
2021-03-23 00:09:08 +0200

    aded new section

---
+++ app.js
@@ -0,0 +1,42 @@
+// let tabItems =document.querySelectorAll(".point");
+// let tabItemWrap =document.querySelector(".chose-point");
+// let tabContent = document.querySelectorAll(".none");
+
+
+
+// function hideContent() {
+//     tabContent.forEach(tab =>{
+//         tab.classList.add("hide");
+//         tab.classList.remove("show");
+       
+//     })
+//     tabItems.forEach(item => item.classList.remove("red"))
+// }
+
+// function showContent(i = 0) {
+//     tabContent[i].classList.add("show")
+//     tabContent[i].classList.remove("hide")
+//     tabItems[i].classList.add("red");
+// }
+
+
+
+// hideContent();
+// showContent();
+// console.log(tabContent);
+
+// tabItemWrap.addEventListener("click", function(e){
+//     let target = e.target;
+//     console.log(target);
+
+//     if (target.classList.contains("point")){
+//         tabItems.forEach((item, i) =>{
+//             if(item === target) {
+//                 hideContent();
+//                 showContent(i)
+                
+//             }
+//         })
+//     }
+
+// });
