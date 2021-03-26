65116cc1478dd12823200fc036fc7f37f392571b
Alex <focus2204@ukr.net>
2021-03-26 21:24:11 +0200

    aded tabs

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-// let tabItems =document.querySelectorAll(".point");
-// let tabItemWrap =document.querySelector(".chose-point");
-// let tabContent = document.querySelectorAll(".none");
+ let tabItems =document.querySelectorAll(".tab-img");
+ let tabItemWrap =document.querySelector(".tab-imgs");
+ let tabContent = document.querySelectorAll(".tab-content");
 
 
 
-// function hideContent() {
-//     tabContent.forEach(tab =>{
-//         tab.classList.add("hide");
-//         tab.classList.remove("show");
+ function hideContent() {
+     tabContent.forEach(tab =>{
+         tab.classList.add("hide");
+         tab.classList.remove("show");
        
-//     })
-//     tabItems.forEach(item => item.classList.remove("red"))
-// }
+     })
+     tabItems.forEach(item => item.classList.remove("red"))
+ }
 
-// function showContent(i = 0) {
-//     tabContent[i].classList.add("show")
-//     tabContent[i].classList.remove("hide")
-//     tabItems[i].classList.add("red");
-// }
+ function showContent(i = 0) {
+     tabContent[i].classList.add("show")
+     tabContent[i].classList.remove("hide")
+     tabItems[i].classList.add("red");
+ }
 
 
 
-// hideContent();
-// showContent();
-// console.log(tabContent);
+ hideContent();
+ showContent();
+ console.log(tabContent);
 
-// tabItemWrap.addEventListener("click", function(e){
-//     let target = e.target;
-//     console.log(target);
+ tabItemWrap.addEventListener("click", function(e){
+     let target = e.target;
+     console.log(target);
 
-//     if (target.classList.contains("point")){
-//         tabItems.forEach((item, i) =>{
-//             if(item === target) {
-//                 hideContent();
-//                 showContent(i)
+     if (target.classList.contains("tab-img")){
+         tabItems.forEach((item, i) =>{
+             if(item === target) {
+                hideContent();
+                 showContent(i)
                 
-//             }
-//         })
-//     }
+            }
+         })
+     }
 
-// });
+ });
 
 $('.block-slider').slick({
     dots: true,
